d73b179b327ffc8016107959edeb74f21dec7bbe
Paul-Henri Leobon <25308170+paulhenri-l@users.noreply.github.com>
2021-10-27 11:40:28 +0200

    Pass payload inside object

--- src/Drivers/EventBridgeDriver.php
+++ src/Drivers/EventBridgeDriver.php
@@ -37,7 +37,7 @@ class EventBridgeDriver implements DriverInterface
         $this->eventBridgeClient->putEvents([
             'Entries' => [
                 array_filter([
-                    'Detail' => json_encode($payload),
+                    'Detail' => ['payload' => json_encode($payload)],
                     'DetailType' => $this->config['prefix'] . $name,
                     'EventBusName' => $this->config['bus_name'],
                     'Source' => $this->config['source'],
